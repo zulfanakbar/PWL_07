b2f79e0652ffdc5c5b9f78030097d6a50c18e770
Ini Zuplek <zulfancoy82@gmail.com>
2021-03-28 22:01:58 +0700

    edit controller

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -1,85 +1,78 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
-
 class MahasiswaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-}
+ /**
+ * Display a listing of the resource.
+ *
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Http\Request;
+Route::resource('mahasiswa', MahasiswaController::class); * @return \Illuminate\Http\Response
+ */
+ public function index()
+ {
+ //fungsi eloquent menampilkan data menggunakan pagination
+ $mahasiswas = Mahasiswa::all(); // Mengambil semua isi tabel
+ $posts = Mahasiswa::orderBy('Nim', 'desc')->paginate(6);
+ return view('mahasiswas.index', compact('mahasiswas'));
+ with('i', (request()->input('page', 1) - 1) * 5);
+ }
+ public function create()
+ {
+ return view('mahasiswas.create');
+ }
+ public function store(Request $request)
+ {
+ //melakukan validasi data
+ $request->validate([
+ 'Nim' => 'required',
+ 'Nama' => 'required',
+ 'Kelas' => 'required',
+ 'Jurusan' => 'required',
+ 'No_Handphone' => 'required',
+ ]);
+ //fungsi eloquent untuk menambah data
+ Mahasiswa::create($request->all());
+ //jika data berhasil ditambahkan, akan kembali ke halaman utama
+ return redirect()->route('mahasiswas.index')
+ ->with('success', 'Mahasiswa Berhasil Ditambahkan');
+ }
+ public function show($Nim)
+ {
+ //menampilkan detail data dengan menemukan/berdasarkan Nim Mahasiswa
+ $Mahasiswa = Mahasiswa::find($Nim);
+ return view('mahasiswas.detail', compact('Mahasiswa'));
+ }
+ public function edit($Nim)
+ {
+//menampilkan detail data dengan menemukan berdasarkan Nim Mahasiswa untuk 
+diedit
+ $Mahasiswa = Mahasiswa::find($Nim);
+ return view('mahasiswas.edit', compact('Mahasiswa'));
+ }
+ public function update(Request $request, $Nim)
+ {//melakukan validasi data
+ $request->validate([
+ 'Nim' => 'required',
+ 'Nama' => 'required',
+ 'Kelas' => 'required',
+ 'Jurusan' => 'required',
+ 'No_Handphone' => 'required',
+ ]);
+//fungsi eloquent untuk mengupdate data inputan kita
+ Mahasiswa::find($Nim)->update($request->all());
+//jika data berhasil diupdate, akan kembali ke halaman utama
+ return redirect()->route('mahasiswas.index')
+ ->with('success', 'Mahasiswa Berhasil Diupdate');
+ }
+ public function destroy( $Nim)
+ {
+//fungsi eloquent untuk menghapus data
+ Mahasiswa::find($Nim)->delete();
+ return redirect()->route('mahasiswas.index')
+ -> with('success', 'Mahasiswa Berhasil Dihapus');
+ }
+};
