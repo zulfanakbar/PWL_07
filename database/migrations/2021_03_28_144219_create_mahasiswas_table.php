df00d4f8d1d98a694b0f4c949ff9f225ddf9f008
Ini Zuplek <zulfancoy82@gmail.com>
2021-03-28 21:52:19 +0700

    4. edit migration

---
+++ database/migrations/2021_03_28_144219_create_mahasiswas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->string ('nim', 10)->index();
+            $table->string ('nama', 300);
+            $table->string ('kelas', 4);
+            $table->string ('jurusan', 4);
+            $table->string ('NoHP', 12);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
