2cfe1bc3f93d1c44876f47ff703bc388f4404242
Ini Zuplek <zulfancoy82@gmail.com>
2021-03-28 22:03:03 +0700

    edit model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,43 +1,25 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
+use Illuminate\Database\Eloquent\Model; //Model Eloquent
+class Mahasiswa extends Model //Definisi Model
 {
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-}
+ protected $table="mahasiswa"; // Eloquent akan membuat model mahasiswa 
+menyimpan record di tabel mahasiswas
+ public $timestamps= false; 
+ protected $primaryKey = 'nim'; // Memanggil isi DB Dengan primarykey
+ /**
+ * The attributes that are mass assignable. *
+ * @var array
+ */
+ protected $fillable = [
+ 'Nim',
+ 'Nama',
+ 'Kelas',
+ 'Jurusan',
+ 'No_Handphone',
+ ];
+};
